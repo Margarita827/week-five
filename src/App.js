395688534b0190599c6651f3e363ce1d4d6afa56
Margarita827 <margaiba5@gmail.com>
2024-04-08 22:25:04 +0100

    added footer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
-import logo from './logo.svg';
+import React from "react";
+
 import './App.css';
 
 export default function App() {
   return (
     <div className="App">
-      
-   Weather App
+      <h1> Weather App</h1>
+      <footer>
+        This project has been coded by Margarita Ibanez and is{" "}
+        <a href="https://github.com/Margarita827/week-five" target="_blank">
+          open-sourced on GitHub
+        </a>
+      </footer>
     </div>
   );
 }
