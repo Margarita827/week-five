515f8d694b93cb7a31bbfd14fdd3f16155fddc67
Margarita827 <margaiba5@gmail.com>
2024-04-10 21:47:05 +0100

    add rel

--- src/App.js
+++ src/App.js
@@ -8,7 +8,11 @@ export default function App() {
       <h1> Weather App</h1>
       <footer>
         This project has been coded by Margarita Ibanez and is{" "}
-        <a href="https://github.com/Margarita827/week-five" target="_blank">
+        <a
+          href="https://github.com/Margarita827/week-five"
+          target="_blank"
+          rel="noreferrer"
+        >
           open-sourced on GitHub
         </a>
       </footer>
