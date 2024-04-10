7815f503a85a547a9e4c87dc9bb1a421444c8165
Margarita827 <margaiba5@gmail.com>
2024-04-10 22:13:46 +0100

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -5,17 +5,19 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-      <h1> Weather App</h1>
-      <footer>
-        This project has been coded by Margarita Ibanez and is{" "}
-        <a
-          href="https://github.com/Margarita827/week-five"
-          target="_blank"
-          rel="noreferrer"
-        >
-          open-sourced on GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <h1> Weather App</h1>
+        <footer>
+          This project has been coded by Margarita Ibanez and is{" "}
+          <a
+            href="https://github.com/Margarita827/week-five"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
