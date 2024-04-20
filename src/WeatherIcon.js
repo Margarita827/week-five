fc7c1a2bd6cffa99ab0ae4724705a6cb0d490639
Margarita827 <margaiba5@gmail.com>
2024-04-20 17:51:05 +0100

    Update WeatherIcon.js

--- src/WeatherIcon.js
+++ src/WeatherIcon.js
@@ -2,6 +2,9 @@ import React from "react";
 
 export default function WeatherIcon(props) {
     return (
-    <img src="http://darkskyapp.github.io/skycons/" alt={props.alt} />
+      <img
+        src="http://darkskyapp.github.io/skycons/import ReactAnimatedWeather from 'react-animated-weather';"
+        alt={props.alt}
+      />
     );
 }
