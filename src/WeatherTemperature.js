53adf96831bd988fb2d35018fbf158729c54d3ee
Margarita827 <margaiba5@gmail.com>
2024-04-21 21:28:35 +0100

    preventDefault

--- src/WeatherTemperature.js
+++ src/WeatherTemperature.js
@@ -3,7 +3,7 @@ import React, { useState } from "react";
 export default function WeatherTemperature(props) {
    const [unit, setUnit] = useState("celsius");
   function convertToFahrenheit(event) {
-    event.preDefault();
+    event.preventDefault();
     setUnit("fahrenheit");
   }
 
