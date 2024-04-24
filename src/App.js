843dd2dafcb24f45b2638001160fca61571315da
Margarita827 <margaiba5@gmail.com>
2024-04-24 06:37:26 +0100

    rel noopener noreferrer

--- src/App.js
+++ src/App.js
@@ -8,14 +8,13 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-     
         <Weather defaultCity="Brighton" />
         <footer>
           This project has been coded by Margarita Ibanez and is{" "}
           <a
             href="https://github.com/Margarita827/week-five"
             target="_blank"
-            rel="noreferrer"
+            rel="noopener noreferrer"
           >
             open-sourced on GitHub
           </a>
